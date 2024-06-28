cb4015e054b843af551651ad1480cd0b02b8cbcb
Bhautik Navdariya <navadariyabhautikm@gmail.com>
2024-06-28 18:26:03 +0530

    change  copy svg icon

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ voiceSelectOption.addEventListener('click', (e) => {
     addToLocalStorage({ voice: classIndex });
 })
 nextBtn.addEventListener('click', () => { increaseRoll() })
-repeatBtn.addEventListener('click', ()=>{
+repeatBtn.addEventListener('click', () => {
     repeatRoll();
 })
 absentBtn.addEventListener('click', addAbsent);
@@ -120,7 +120,35 @@ audioBtn.addEventListener('click', () => {
 copyBtn.addEventListener('click', () => {
     navigator.clipboard.writeText(absentRolls.value);
     copyBtn.innerHTML = '<svg width="25px" height="25px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="none"><path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 5L8 15l-5-4"/></svg>'
-    setTimeout(() => { copyBtn.innerHTML = '<svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M21 8C21 6.34315 19.6569 5 18 5H10C8.34315 5 7 6.34315 7 8V20C7 21.6569 8.34315 23 10 23H18C19.6569 23 21 21.6569 21 20V8ZM19 8C19 7.44772 18.5523 7 18 7H10C9.44772 7 9 7.44772 9 8V20C9 20.5523 9.44772 21 10 21H18C18.5523 21 19 20.5523 19 20V8Z" fill="#0F0F0F"/><path d="M6 3H16C16.5523 3 17 2.55228 17 2C17 1.44772 16.5523 1 16 1H6C4.34315 1 3 2.34315 3 4V18C3 18.5523 3.44772 19 4 19C4.55228 19 5 18.5523 5 18V4C5 3.44772 5.44772 3 6 3Z" fill="#0F0F0F"/></svg>' }, 2000);
+    setTimeout(() => {
+        copyBtn.innerHTML = `<svg fill="#000000" width="25px" height="25px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+    viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve">
+<g id="Text-files">
+   <path d="M53.9791489,9.1429005H50.010849c-0.0826988,0-0.1562004,0.0283995-0.2331009,0.0469999V5.0228
+       C49.7777481,2.253,47.4731483,0,44.6398468,0h-34.422596C7.3839517,0,5.0793519,2.253,5.0793519,5.0228v46.8432999
+       c0,2.7697983,2.3045998,5.0228004,5.1378999,5.0228004h6.0367002v2.2678986C16.253952,61.8274002,18.4702511,64,21.1954517,64
+       h32.783699c2.7252007,0,4.9414978-2.1725998,4.9414978-4.8432007V13.9861002
+       C58.9206467,11.3155003,56.7043495,9.1429005,53.9791489,9.1429005z M7.1110516,51.8661003V5.0228
+       c0-1.6487999,1.3938999-2.9909999,3.1062002-2.9909999h34.422596c1.7123032,0,3.1062012,1.3422,3.1062012,2.9909999v46.8432999
+       c0,1.6487999-1.393898,2.9911003-3.1062012,2.9911003h-34.422596C8.5049515,54.8572006,7.1110516,53.5149002,7.1110516,51.8661003z
+        M56.8888474,59.1567993c0,1.550602-1.3055,2.8115005-2.9096985,2.8115005h-32.783699
+       c-1.6042004,0-2.9097996-1.2608986-2.9097996-2.8115005v-2.2678986h26.3541946
+       c2.8333015,0,5.1379013-2.2530022,5.1379013-5.0228004V11.1275997c0.0769005,0.0186005,0.1504021,0.0469999,0.2331009,0.0469999
+       h3.9682999c1.6041985,0,2.9096985,1.2609005,2.9096985,2.8115005V59.1567993z"/>
+   <path d="M38.6031494,13.2063999H16.253952c-0.5615005,0-1.0159006,0.4542999-1.0159006,1.0158005
+       c0,0.5615997,0.4544001,1.0158997,1.0159006,1.0158997h22.3491974c0.5615005,0,1.0158997-0.4542999,1.0158997-1.0158997
+       C39.6190491,13.6606998,39.16465,13.2063999,38.6031494,13.2063999z"/>
+   <path d="M38.6031494,21.3334007H16.253952c-0.5615005,0-1.0159006,0.4542999-1.0159006,1.0157986
+       c0,0.5615005,0.4544001,1.0159016,1.0159006,1.0159016h22.3491974c0.5615005,0,1.0158997-0.454401,1.0158997-1.0159016
+       C39.6190491,21.7877007,39.16465,21.3334007,38.6031494,21.3334007z"/>
+   <path d="M38.6031494,29.4603004H16.253952c-0.5615005,0-1.0159006,0.4543991-1.0159006,1.0158997
+       s0.4544001,1.0158997,1.0159006,1.0158997h22.3491974c0.5615005,0,1.0158997-0.4543991,1.0158997-1.0158997
+       S39.16465,29.4603004,38.6031494,29.4603004z"/>
+   <path d="M28.4444485,37.5872993H16.253952c-0.5615005,0-1.0159006,0.4543991-1.0159006,1.0158997
+       s0.4544001,1.0158997,1.0159006,1.0158997h12.1904964c0.5615025,0,1.0158005-0.4543991,1.0158005-1.0158997
+       S29.0059509,37.5872993,28.4444485,37.5872993z"/>
+</g>
+</svg>` }, 2000);
 
 })
 downloadBtn.addEventListener('click', () => {
@@ -275,14 +303,14 @@ function speekRollNumber(rollNumber) {
 // }
 // absentRolls.innerHTML = outString;
 // }
-function repeatRoll(){
-    speaker.text = roll%100;
+function repeatRoll() {
+    speaker.text = roll % 100;
     speechSynthesis.speak(speaker);
 }
-function handlePresntRoll(e){
-    if(e.key == 'Enter'){
+function handlePresntRoll(e) {
+    if (e.key == 'Enter') {
         let tempRoll = e.target.value;
-        if(tempRoll.length != 3){
+        if (tempRoll.length != 3) {
             return;
         }
         e.target.value = "";
@@ -293,15 +321,15 @@ function handlePresntRoll(e){
         updateTextArea();
     }
 }
-function handleAbsentRoll(e){
-    if(e.key == 'Enter'){
+function handleAbsentRoll(e) {
+    if (e.key == 'Enter') {
         let tempRoll = e.target.value;
-        if(tempRoll.length != 3){
+        if (tempRoll.length != 3) {
             return;
         }
         e.target.value = "";
         tempRoll = parseInt(tempRoll);
-        if(isDuplicate(absents, tempRoll)) return;
+        if (isDuplicate(absents, tempRoll)) return;
         absents.push(tempRoll);
         absents.sort();
         updateTextArea();
